6eecda62c88eb8b2f88dc3a609ea17932127845c
thisisaaronland <aaron@localhost>
2021-07-30 10:01:19 -0700

    try indexing only names

--- index.go
+++ index.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"time"
 	"log"
+	"strings"
 )
 
 func IndexReader(ctx context.Context, index bleve.Index, r io.Reader) error {
@@ -25,21 +26,45 @@ func IndexReader(ctx context.Context, index bleve.Index, r io.Reader) error {
 		return err
 	}
 
-	rsp := gjson.GetBytes(body, "properties")
+	props_rsp := gjson.GetBytes(body, "properties")
 
-	if !rsp.Exists() {
+	if !props_rsp.Exists() {
 		return fmt.Errorf("Document is missing properties")
 	}
 
-	id_rsp := rsp.Get("wof:id")
+	id_rsp := props_rsp.Get("wof:id")
 
 	if !id_rsp.Exists() {
 		return fmt.Errorf("Missing wof:id property")
 	}
 
 	str_id := id_rsp.String()
-	doc := rsp.Value()
 
+	// START OF determine what to index
+	
+	names := make([]string, 0)
+
+	for k,v := range props_rsp.Map() {
+
+		if k == "wof:name" {
+			names = append(names, v.String())
+			continue
+		}
+
+		if strings.HasPrefix(k, "name:"){
+
+			for _, n := range v.Array(){
+				names = append(names, n.String())
+			}
+		}
+	}
+	
+	doc := map[string]interface{}{
+		"names": names,
+	}
+
+	// END OF determine what to index
+	
 	t1 := time.Now()
 	
 	defer func(){
